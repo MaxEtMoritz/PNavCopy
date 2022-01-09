987a9236da1f35d6c649065133b337f2b38dd2f3
MaxEtMoritz <markus@memotech.franken.de>
2022-01-09 21:17:52 +0100

    Bot: fix logging
    lock the logging task as long as logging is in progress to mitigate "file is used by another process" issue and prevent the console logs from interfering.

--- CompanionBot/CompanionBot/Logger.cs
+++ CompanionBot/CompanionBot/Logger.cs
@@ -9,57 +9,61 @@ namespace CompanionBot
 {
     public class Logger
     {
+        public static object Lock = new Object();
         public Task Log(LogMessage message)
         {
-            string logEntry = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss") + " " + message.Source.PadRight(11).Substring(0, 11) + " ";
-            Console.Write(logEntry);
-            switch (message.Severity)
+            lock (Lock)
             {
-                case LogSeverity.Critical:
-                    Console.ForegroundColor = ConsoleColor.DarkRed;
-                    break;
-                case LogSeverity.Error:
-                    Console.ForegroundColor = ConsoleColor.Red;
-                    break;
-                case LogSeverity.Warning:
-                    Console.ForegroundColor = ConsoleColor.DarkYellow;
-                    break;
-                case LogSeverity.Info:
-                    break;
-                case LogSeverity.Verbose:
-                    break;
-                case LogSeverity.Debug:
-                    break;
-                default:
-                    break;
-            }
-            Console.Write(message.Severity.ToString().PadRight(8));
-            logEntry += message.Severity.ToString().PadRight(8);
-            Console.ForegroundColor = ConsoleColor.Gray;
-            Console.Write(" " + message.Message);
-            logEntry += " " + message.Message;
-            if (message.Exception != null)
-            {
-                Console.WriteLine(" " + message.Exception);
-                logEntry += " " + message.Exception;
-            }
-            else
-            {
-                Console.WriteLine();
-            }
-            try
-            {
-                File.AppendAllText("PNavCopy_Log.txt", logEntry+"\n");
-            }
-            catch(Exception e)
-            {
-                Console.Write($"{DateTime.Now:yyyy-MM-dd HH:mm:ss} Logger      ");
-                Console.ForegroundColor = ConsoleColor.DarkRed;
-                Console.Write("Critical ");
+                string logEntry = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss") + " " + message.Source.PadRight(11).Substring(0, 11) + " ";
+                Console.Write(logEntry);
+                switch (message.Severity)
+                {
+                    case LogSeverity.Critical:
+                        Console.ForegroundColor = ConsoleColor.DarkRed;
+                        break;
+                    case LogSeverity.Error:
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        break;
+                    case LogSeverity.Warning:
+                        Console.ForegroundColor = ConsoleColor.DarkYellow;
+                        break;
+                    case LogSeverity.Info:
+                        break;
+                    case LogSeverity.Verbose:
+                        break;
+                    case LogSeverity.Debug:
+                        break;
+                    default:
+                        break;
+                }
+                Console.Write(message.Severity.ToString().PadRight(8));
+                logEntry += message.Severity.ToString().PadRight(8);
                 Console.ForegroundColor = ConsoleColor.Gray;
-                Console.WriteLine($"Unable to write to Log File. {e}");
+                Console.Write(" " + message.Message);
+                logEntry += " " + message.Message;
+                if (message.Exception != null)
+                {
+                    Console.WriteLine(" " + message.Exception);
+                    logEntry += " " + message.Exception;
+                }
+                else
+                {
+                    Console.WriteLine();
+                }
+                try
+                {
+                    File.AppendAllText("PNavCopy_Log.txt", logEntry + "\n");
+                }
+                catch (Exception e)
+                {
+                    Console.Write($"{DateTime.Now:yyyy-MM-dd HH:mm:ss} Logger      ");
+                    Console.ForegroundColor = ConsoleColor.DarkRed;
+                    Console.Write("Critical ");
+                    Console.ForegroundColor = ConsoleColor.Gray;
+                    Console.WriteLine($"Unable to write to Log File. {e}");
+                }
+                return Task.CompletedTask;
             }
-            return Task.CompletedTask;
         }
     }
 }
